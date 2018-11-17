4e9dc961700369f2bc665d0b08538de76c0c7c46
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-17 21:08:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:recipe_id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
